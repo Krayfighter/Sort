07e7ac76918b2f9ee73058d178e9ec87a4d91a16
Aiden Kring <aidenjkring@gmail.com>
2021-04-28 13:48:00 -0400

    edit to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+
+template <typename T>
+struct ListItem {
+    vector<T> _item;
+    int _location;
+    ListItem(int location, vector<T> item = {}) {
+        _location = location;
+        _item = item;
+    }
+}
+
+template <typename T>
+class Sort {
+    vector<ListItem<T>> sorter;
+    vector
+    public:
+        Sort()
+}
 
 int main() {
     cout << "hello world!" << endl;
